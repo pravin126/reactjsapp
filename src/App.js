b32d6f68bee8dccb4f2a560cebb401941b15fa9f
pravin126 <pravinkalokhe5@.com>
2023-10-09 17:24:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{ Component } from "react";
+
+
+class App extends Component {
+    render(){
+        return (
+            <React.Fragment>
+                <h1>Hello BSS</h1>
+                <h2>Hello BSS2</h2>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App ;
+
